7ff345a2fa08109b56952fe7f6cbdf9bb6dc9f7d
OmarEialsalman <omareialsalman07@gmail.com>
2026-07-25 01:56:02 +0300

    put all test method that related to people managment to class and them general method

---
+++ DVLD_Console/PeopleManagementTest.cs
@@ -0,0 +1,162 @@
+﻿using DVLD_Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Console
+{
+    internal static class PeopleManagementTest
+    {
+        internal static void PrintPerson(Person person)
+        {
+            if (person == null)
+            {
+                Console.WriteLine("Person not found.");
+                return;
+            }
+
+            Console.WriteLine("------------ Person Card -------------");
+            Console.WriteLine("ID                     : " + person.ID);
+            Console.WriteLine("First Name             : " + person.FirstName);
+            Console.WriteLine("Second Name            : " + person.SecondName);
+            Console.WriteLine("Third Name             : " + person.ThirdName);
+            Console.WriteLine("Last Name              : " + person.LastName);
+            Console.WriteLine("Birth of Date          : " + person.DateOfBirth.ToShortDateString());
+            Console.WriteLine("Gender                 : " + person.Gendor);
+            Console.WriteLine("Phone                  : " + person.Phone);
+            Console.WriteLine("Email                  : " + person.Email);
+            Console.WriteLine("Nationality Country ID : " + person.NationalityCountryID);
+            Console.WriteLine("Image Path             : " + person.ImagePath);
+            Console.WriteLine("--------------------------------------");
+        }
+
+        internal static void TestGetPerson(int personID)
+        {
+            try
+            {
+                Console.WriteLine($"\n--- Testing Get Person (ID: {personID}) ---");
+                PrintPerson(PersonService.Find(personID));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : " + ex.ToString());
+            }
+        }
+
+        internal static void TestAddNewPerson(string nationalNo, string firstName, string secondName, string thirdName, string lastName, DateTime dateOfBirth, Person.enGendor gender, string address, string phone, string email, int nationalityCountryID, string imagePath)
+        {
+            try
+            {
+                Console.WriteLine("\n--- Testing Add New Person ---");
+                Person person = new Person(nationalNo, firstName, secondName, thirdName, lastName, dateOfBirth, gender, address, phone, email, nationalityCountryID, imagePath);
+
+                int id = PersonService.AddNewPerson(person);
+
+                if (id != -1)
+                {
+                    Console.WriteLine($"Person added Successfully with ID = {id}!");
+                }
+                else
+                {
+                    Console.WriteLine("Can't add Person :-(");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : " + ex.ToString());
+            }
+        }
+
+        internal static void TestIsExistByID(int personID)
+        {
+            try
+            {
+                Console.WriteLine($"\n--- Testing IsExist By ID ({personID}) ---");
+                if (PersonService.isExist(personID))
+                {
+                    Console.WriteLine($"Person with ID : {personID} exists.");
+                }
+                else
+                {
+                    Console.WriteLine($"Person with ID : {personID} does NOT exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : " + ex.ToString());
+            }
+        }
+
+        internal static void TestIsExistByNationalNo(string nationalNo)
+        {
+            try
+            {
+                Console.WriteLine($"\n--- Testing IsExist By NationalNo ({nationalNo}) ---");
+                if (PersonService.isExist(nationalNo))
+                {
+                    Console.WriteLine($"Person with NationalNo : {nationalNo} exists.");
+                }
+                else
+                {
+                    Console.WriteLine($"Person with NationalNo : {nationalNo} does NOT exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : " + ex.ToString());
+            }
+        }
+
+        internal static void TestUpdatePerson(int personID)
+        {
+            try
+            {
+                Console.WriteLine($"\n--- Testing Update Person (ID: {personID}) ---");
+                Person person = PersonService.Find(personID);
+                if (person == null)
+                {
+                    Console.WriteLine($"There is no person with ID {personID}");
+                    return;
+                }
+
+                person.LastName = "TEST_UPDATED";
+                person.Email = "updated_test@gmail.com";
+
+                if (PersonService.UpdatePerson(person))
+                {
+                    Console.WriteLine("Updated Person Successfully!");
+                }
+                else
+                {
+                    Console.WriteLine("Can't Update Person!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : " + ex.ToString());
+            }
+        }
+
+        internal static void TestDeletePerson(int personID)
+        {
+            try
+            {
+                Console.WriteLine($"\n--- Testing Delete Person (ID: {personID}) ---");
+                if (PersonService.DeletePerson(personID))
+                {
+                    Console.WriteLine("Deleted Person successfully!");
+                }
+                else
+                {
+                    Console.WriteLine("Can't Delete Person!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : " + ex.ToString());
+            }
+        }
+    }
+}
--- DVLD_Console/Program.cs
+++ DVLD_Console/Program.cs
@@ -9,150 +9,23 @@ namespace DVLD_Console
 {
     internal class Program
     {
-        static void PrintPerson(in Person person)
-        {
-            if (person == null)
-                return;
-
-            Console.WriteLine("------------Person Card --------- ");
-            Console.WriteLine("ID                     : " + person.ID);
-            Console.WriteLine("First Name             : " + person.FirstName);
-            Console.WriteLine("Seconed Name           : " + person.SecondName);
-            Console.WriteLine("Third Name             : " + person.ThirdName);
-            Console.WriteLine("Last Name              : " + person.LastName);
-            Console.WriteLine("Birth of Date          : " + person.DateOfBirth);
-            Console.WriteLine("Gendor                 : " + person.Gendor);
-            Console.WriteLine("Phone                  : " + person.Phone);
-            Console.WriteLine("Email                  : " + person.Email);
-            Console.WriteLine("Nationality Country ID : " + person.NationalityCountryID);
-            Console.WriteLine("Image Path             : " + person.ImagePath);
-        }
-        static void testGetPerson()
-        {
-            try
-            {
-                PrintPerson(PersonService.Find(1));
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine("Error : " + ex.ToString());
-            }
-        }
-        static void testAddNewPerson()
-        {
-            try
-            {
-                Person person = new Person("N8", "Mohamad", "Maher", "Fadi", "Al-Mamhor", new DateTime(2001, 1, 12),
-                    Person.enGendor.Male, "Amman-am", "0778978856", "mohad@gmail.com", 2, "");
-
-                int id = PersonService.AddNewPerson(person);
-                Console.WriteLine(id);
-
-                if (id != -1)
-                {
-                    Console.WriteLine("Person added Successfully!");
-                }
-                else
-                {
-                    Console.WriteLine("Can't add Person -(");
-                }
-
-
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine("Error : " + ex.ToString());
-            }
-        }
-        static void testIsExistByID()
-        {
-            try
-            {
-                if(PersonService.isExist(1))
-                {
-                    Console.WriteLine("Peron with ID : 1 is exist");
-                }
-                else
-                {
-                    Console.WriteLine("Peron with ID : 1 is NOT exist");
-                }
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine("Error : " + ex.ToString());
-            }
-        }
-        static void testIsExistByNationNo()
-        {
-            try
-            {
-                if (PersonService.isExist("N1"))
-                {
-                    Console.WriteLine("Peron with NationalNo : N1 is exist");
-                }
-                else
-                {
-                    Console.WriteLine("Peron with NationalNo : N1 is NOT exist");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error : " + ex.ToString());
-            }
-        }
-        static void testUpdatePerson()
-        {
-            try
-            {
-                Person person = PersonService.Find(4);
-                if (person == null)
-                {
-                    Console.WriteLine("There is no person with ID 4");
-                    return;
-                }
-
-                person.LastName = "TEST";
-                person.Email = "TEST@TEST";
-                if(PersonService.UpdatePerson(person))
-                {
-                    Console.WriteLine("Updated Person Successfully!");
-                }
-                else
-                {
-                    Console.WriteLine("Can't Update Person!");
-                }
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine("Error : " + ex.ToString());
-            }
-        }
-        static void testDeletePerson()
-        {
-            try
-            {
-                if(PersonService.DeletePerson(4))
-                {
-                    Console.WriteLine("Deleted Peron successfully!");
-                }
-                else
-                {
-                    Console.WriteLine("Can't Delete Person!");
-                }
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine("Error : " + ex.ToString());
-            }
-        }
+        
         static void Main(string[] args)
         {
-            //testGetPerson();
-            //testAddNewPerson();
-            //testIsExistByID();
-            //testIsExistByNationNo();
-            //testUpdatePerson();
-            testDeletePerson();
+            //PeopleManagementTest.TestIsExistByID(1);
+            //PeopleManagementTest.TestGetPerson(1);
+
+            /*PeopleManagementTest.TestAddNewPerson(
+                "N99", "Ali", "Khaled", "Sami", "Hassan",
+                new DateTime(1998, 5, 20), Person.enGendor.Male,
+                "Irbid", "0791111111", "ali@mail.com", 1, ""
+            );*/
+
+            //PeopleManagementTest.TestIsExistByID(1);
+            //PeopleManagementTest.TestIsExistByNationalNo("N1");
+            //PeopleManagementTest.TestUpdatePerson(2);
+            PeopleManagementTest.TestDeletePerson(2);
+
 
             Console.ReadLine();
         }
