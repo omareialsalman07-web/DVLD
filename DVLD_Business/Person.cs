85d42497e18a9ce96e748ae1e79d0704d1475688
OmarEialsalman <omareialsalman07@gmail.com>
2026-07-24 03:39:23 +0300

    create person class witch has person's data and PersonService class with will talk to data access

---
+++ DVLD_Business/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Business
+{
+    public class Person
+    {
+        public int ID { get; }
+        public string NationalNo { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string ThirdName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Gendor { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public int NationalityCountryID { get; set; }
+        public string ImagePath { get; set; }
+
+        public int Age()
+        {
+            int age = DateTime.Today.Year - DateOfBirth.Year;
+
+            if (DateOfBirth.Date > DateTime.Today.AddYears(-age))
+            {
+                age--;
+            }
+
+            return age;
+        }
+
+        public Person(string NationalNo, string FirstName,  string SecondName, string ThirdName, 
+        string LastName, DateTime DateOfBirth, int Gendor, string Address, string Phone, string Email, 
+        int NationalityCountryID, string ImagePath)
+        {
+            this.NationalNo = NationalNo;
+            this.FirstName = FirstName;
+            this.SecondName = SecondName;
+            this.ThirdName = ThirdName;
+            this.DateOfBirth = DateOfBirth;
+            this.Gendor = Gendor;
+            this.Address = Address;
+            this.Phone = Phone;
+            this.Email = Email;
+            this.NationalityCountryID = NationalityCountryID;
+            this.ImagePath = ImagePath;
+        }
+
+        public Person(int ID, string NationalNo, string FirstName,  string SecondName, string ThirdName, 
+        string LastName, DateTime DateOfBirth, int Gendor, string Address, string Phone, string Email, 
+        int NationalityCountryID, string ImagePath) 
+            : this(NationalNo, FirstName, SecondName, ThirdName, LastName, DateOfBirth, Gendor, Address, Phone, 
+                  Email, NationalityCountryID, ImagePath)
+        {
+            this.ID = ID;
+        }
+    }
+}
---
+++ DVLD_Business/PersonService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using DVLD_DataAccess;
+
+namespace DVLD_Business
+{
+    public static class PersonService
+    {
+        public static Person Find(int ID)
+        {
+            Person person = null;
+
+            string NationalNo = "", FirstName = "", SecondName = "", ThirdName = "", LastName = "", Address = "",
+                Phone = "", Email = "", ImagePath = "";
+            DateTime DateOfBirth= DateTime.Now;  
+            int Gendor = -1, NationalityCountryID = -1;
+
+            try
+            {
+                if(PersonData.GetPersonByID(ID, ref NationalNo, ref FirstName, ref SecondName, ref ThirdName, ref LastName,
+                    ref DateOfBirth, ref Gendor, ref Address, ref Phone, ref Email, ref NationalityCountryID, ref ImagePath))
+                {
+                    person = new Person(ID, NationalNo, FirstName, SecondName, ThirdName, LastName, DateOfBirth, Gendor, Address,
+                        Phone, Email, NationalityCountryID, ImagePath);
+                }
+            }
+            catch
+            {
+                person = null;
+                throw;
+            }
+
+            return person;
+        }
+    }
+}
