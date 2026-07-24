b8815cec7573def488b7096e40a132551e8c70ec
OmarEialsalman <omareialsalman07@gmail.com>
2026-07-25 00:08:03 +0300

    - fix Person constructor by filling lastName - make gendor based on enum instead of int

--- DVLD_Business/Person.cs
+++ DVLD_Business/Person.cs
@@ -8,6 +8,8 @@ namespace DVLD_Business
 {
     public class Person
     {
+        public enum enGendor { Male = 0, eFemale = 1}
+
         public int ID { get; }
         public string NationalNo { get; set; }
         public string FirstName { get; set; }
@@ -15,7 +17,7 @@ namespace DVLD_Business
         public string ThirdName { get; set; }
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public int Gendor { get; set; }
+        public enGendor Gendor { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
@@ -35,13 +37,14 @@ namespace DVLD_Business
         }
 
         public Person(string NationalNo, string FirstName,  string SecondName, string ThirdName, 
-        string LastName, DateTime DateOfBirth, int Gendor, string Address, string Phone, string Email, 
+        string LastName, DateTime DateOfBirth, enGendor Gendor, string Address, string Phone, string Email, 
         int NationalityCountryID, string ImagePath)
         {
             this.NationalNo = NationalNo;
             this.FirstName = FirstName;
             this.SecondName = SecondName;
             this.ThirdName = ThirdName;
+            this.LastName = LastName;
             this.DateOfBirth = DateOfBirth;
             this.Gendor = Gendor;
             this.Address = Address;
@@ -52,7 +55,7 @@ namespace DVLD_Business
         }
 
         public Person(int ID, string NationalNo, string FirstName,  string SecondName, string ThirdName, 
-        string LastName, DateTime DateOfBirth, int Gendor, string Address, string Phone, string Email, 
+        string LastName, DateTime DateOfBirth, enGendor Gendor, string Address, string Phone, string Email, 
         int NationalityCountryID, string ImagePath) 
             : this(NationalNo, FirstName, SecondName, ThirdName, LastName, DateOfBirth, Gendor, Address, Phone, 
                   Email, NationalityCountryID, ImagePath)
