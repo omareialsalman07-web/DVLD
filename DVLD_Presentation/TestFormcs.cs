5462f2293d0bbd3f2061339a1b765e98459cfc0d
OmarEialsalman <omareialsalman07@gmail.com>
2026-07-25 14:55:37 +0300

    add TestForm for testing

--- DVLD_Presentation/TestFormcs.cs
+++ DVLD_Presentation/TestFormcs.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace DVLD_Presentation
 {
-    public partial class Form1 : Form
+    public partial class TestFormcs : Form
     {
-        public Form1()
+        public TestFormcs()
         {
             InitializeComponent();
         }
