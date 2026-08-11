d9857f2699dde7e1bb7382181c9a868b6011720d
OmarEialsalman <omareialsalman07@gmail.com>
2026-08-11 20:23:00 +0300

    Add User class and UserService class in business layer to talk with UserData

---
+++ DVLD_Business/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Business
+{
+    public class User
+    {
+        public int UserID { get; }
+        public int PersonID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        
+        public User(int PersonID, string UserName, string Password)
+        {
+            this.PersonID = PersonID;
+            this.UserName = UserName;
+            this.Password = Password;
+        }
+
+        public User(int UserID, int PersronID, string UserName, string Password) : this(PersronID, UserName, Password)
+        {
+            this.UserID = UserID;
+        }
+    }
+}
---
+++ DVLD_Business/UserService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DVLD_DataAccess;
+
+namespace DVLD_Business
+{
+    public static class UserService
+    {
+        public static int AddNewUser(in User user)
+        {
+            int _id = -1;
+
+            try
+            {
+                _id = UserData.AddNewUser(user.PersonID, user.UserName, user.Password);
+            }
+            catch(Exception ex)
+            {
+                _id = -1;
+                throw;
+            }
+
+            return _id;
+        }
+    }
+}
